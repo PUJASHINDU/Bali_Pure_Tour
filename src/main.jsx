4c4c96048b57c35d525e16c58f8f4bacd1f2224b
Shindu Astawa <shinduastawa@gmail.com>
2025-02-27 13:14:26 +0800

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './Context/AuthContext';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <AuthProvider>
         <App />
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
