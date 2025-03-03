b48ddfd1b559be6f21c59e54e02896b998724aa0
Shindu Astawa <shinduastawa@gmail.com>
2025-03-03 16:31:56 +0800

    New

--- Backend/models/TransactionModel.js
+++ Backend/models/TransactionModel.js
@@ -1,36 +1,51 @@
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/Database.js';
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Booking from "./BookingModel.js"; // Import Booking
 
-export const Transaction = sequelize.define('Transaction', {
-    transaction_id: {
-        type: DataTypes.STRING,
-        primaryKey: true
+const { DataTypes } = Sequelize;
+
+const Transaction = db.define(
+  "transaction",
+  {
+    id_transaction: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    customer_name: {
-        type: DataTypes.STRING,
-        allowNull: false
+    id_booking: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Booking",
+        key: "id_booking",
+      },
     },
-    customer_email: {
-        type: DataTypes.STRING,
-        allowNull: false
+    total_price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
-    gross_amount: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+    payment_status: {
+      type: DataTypes.ENUM("pending", "paid", "failed"),
+      defaultValue: "pending",
+      allowNull: false,
     },
-    status: {
-        type: DataTypes.ENUM('PENDING', 'PAID', 'CANCELLED'),
-        defaultValue: 'PENDING'
+    payment_method: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    snap_token: {
-        type: DataTypes.STRING,
-        allowNull: true
+    transaction_date: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
     },
-    snap_redirect_url: {
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-}, {
-    tableName: 'transactions',
-    timestamps: true
-});
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+// Relasi dengan Booking
+Transaction.belongsTo(Booking, { foreignKey: "id_booking", as: "Booking" });
+Booking.hasOne(Transaction, { foreignKey: "id_booking", as: "Transaction", onDelete: 'CASCADE' });
+
+export default Transaction;
